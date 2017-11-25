b66f6df2f0654ae1a168041af5c5d8214d6070a4
jariepei <jariepei@gmail.com>
2017-11-25 11:40:50 +0000

    Updated readmes

---
+++ Examples/05.node_docker_build_push/README.md
@@ -0,0 +1,5 @@
+# Script para validar la appliación node, build el container y hacer un push al registro
+
+- Ejemplo de ejecución:
+  - chmod +x build.sh
+  - ./build.sh test 0.1 jariepei
--- Examples/05.node_docker_build_push/src/app.js
+++ Examples/05.node_docker_build_push/src/app.js
@@ -1,6 +1,7 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
+  console.log("Respondiendo al cliente: "+ (new Date().toISOString()));
   res.writeHead(200, { 'Content-type': 'text/plain' });
   res.write('Servidor Web - Ejemplo');
   res.end();
